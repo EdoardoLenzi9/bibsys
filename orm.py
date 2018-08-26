f70f52a3d097602e7265e3ea79b1eda12b5daa9f
edoardo.lenzi@factorymind.com <edoardo.lenzi@factorymind.com>
2018-08-26 14:32:12 +0200

    wip

--- orm.py
+++ orm.py
@@ -4,13 +4,11 @@ from sqlalchemy import Column, Integer, String, create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('mysql+pymysql://{0}:{1}@{2}/{3}'.format('root', 'dba', '127.0.0.1:3306', 'soweego'), pool_recycle=3600, echo=True)
+engine = create_engine('mysql+pymysql://{0}@{1}/{2}'.format('edoardolenzi', '10.68.23.58', 's53821__orm_connection_test'), pool_recycle=3600, echo=True)
 
-if not engine.dialect.has_table(engine, 'stocazzo'):  # If table don't exist, Create.
+if not engine.dialect.has_table(engine, 'test'):  # If table don't exist, Create.
     metadata = MetaData(engine)
-    Table('stocazzo', metadata,
+    Table('test', metadata,
         Column('Id', Integer, primary_key=True, nullable=False), 
-        Column('Country', String),
-        Column('Brand', String),
-    # Implement the creation
+        Column('Name', String),
     metadata.create_all())
