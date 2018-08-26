c28cbe7f2e03763455f65842c036602bc0cd89d1
eddy <edoardolenzi9@gmail.com>
2018-08-26 15:48:58 +0200

    work

--- orm.py
+++ orm.py
@@ -7,14 +7,17 @@ TEST_DB = 's53821__test_index'
 PROD_DB = 's51434__mixnmatch_large_catalogs'
 DODO_DB = 's53821__orm_connection_test'
 HOST = 'tools.db.svc.eqiad.wmflabs'
+USER = 'root'
+PASSWORD = 'dba'
 
+#s53821'@'10.68.23.58'
 
 Base = declarative_base()
-engine = create_engine('mysql://{0}@{1}/{2}'.format('edoardolenzi', 'tools.db.svc.eqiad.wmflabs', 's53821__orm_connection_test'), pool_recycle=3600, echo=True)
+engine = create_engine('mysql+pymysql://root:dba@localhost/soweego', echo=True)
 
-if not engine.dialect.has_table(engine, 'test'):  # If table don't exist, Create.
-    metadata = MetaData(engine)
-    Table('test', metadata,
-        Column('Id', Integer, primary_key=True, nullable=False), 
-        Column('Name', String),
+metadata = MetaData(engine)
+table = Table('test', metadata,
+    Column('Id', Integer, primary_key=True, nullable=False), 
+    Column('Name', Integer),
     metadata.create_all())
+table.create()
