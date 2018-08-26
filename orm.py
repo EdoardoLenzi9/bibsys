d1d4c01275aab15c1298b571622a612ffd3192f7
edoardo.lenzi@factorymind.com <edoardo.lenzi@factorymind.com>
2018-08-26 14:41:07 +0200

    wip

--- orm.py
+++ orm.py
@@ -3,8 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 
+TEST_DB = 's53821__test_index'
+PROD_DB = 's51434__mixnmatch_large_catalogs'
+DODO_DB = 's53821__orm_connection_test'
+HOST = 'tools.db.svc.eqiad.wmflabs'
+
+
 Base = declarative_base()
-engine = create_engine('mysql+pymysql://{0}@{1}/{2}'.format('edoardolenzi', 'local', 's53821__orm_connection_test'), pool_recycle=3600, echo=True)
+engine = create_engine('mysql://{0}@{1}/{2}'.format('edoardolenzi', 'tools.db.svc.eqiad.wmflabs', 's53821__orm_connection_test'), pool_recycle=3600, echo=True)
 
 if not engine.dialect.has_table(engine, 'test'):  # If table don't exist, Create.
     metadata = MetaData(engine)
